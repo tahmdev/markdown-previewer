d7f68438e59d0170c272693f1fffcc9d054d0303
tahmdev <qwertiimusic@gmail.com>
2022-03-08 19:36:25 +0100

    Added hide functionality + minor fixes

--- src/App.js
+++ src/App.js
@@ -17,10 +17,13 @@ class Main extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      input: ""
+      input: "",
+      editorVis: "visible",
+      previewVis: "visible"
     }
     this.markdown = this.markdown.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
   handleChange(event){
     this.setState({
@@ -28,31 +31,70 @@ class Main extends React.Component{
       input: event.target.value
     })
   }
+  handleClick(event){
+    if (this.state.editorVis === "hidden" || this.state.previewVis === "hidden"){
+      this.setState({
+        editorVis: "visible",
+        previewVis: "visible"
+      })
+    }
+    else if (event.target.id === "editor-toggle"){
+      this.setState({
+        previewVis: "hidden"
+      })
+    }
+    else if (event.target.id === "preview-toggle"){
+      this.setState({
+        editorVis: "hidden"
+        
+      })
+    }
+  }
+
   markdown(){
     let rawMarkup = marked.parse(this.state.input, {breaks:true})
     return {__html: rawMarkup};
   }
+
   render(){
+    let editorStyle = {};
+    let editorVis ={}
+    let previewVis ={}
+    if (this.state.previewVis === "hidden"){
+      editorStyle = {
+        minHeight: "90vh",
+        overflowY: "auto",
+      }
+      previewVis = {
+        display: "none"
+      }
+    }
+    if (this.state.editorVis === "hidden"){
+      editorVis = {
+        display: "none"
+      }
+    }
+    
+    console.log(editorVis)
     return(
       <div>
-        <div id="editor-container">
+        <div id="editor-container" style={editorVis}>
           <div id="editor-header"> 
-            <p>Editor</p> 
-            <i className="fa fa-arrows-alt hide-toggle" />
+            <p>Editor {this.state.editorVis == "visible" ? "true" : "false"}</p> 
+            <i className={this.state.previewVis === "visible" ? "fa fa-arrows-alt hide-toggle" : "fa fa-compress hide-toggle"} id="editor-toggle" onClick={this.handleClick}/>
           </div>
-          <textarea id="editor-input" onChange={this.handleChange}></textarea>
+          <textarea id="editor-input" onChange={this.handleChange} style={editorStyle} value={this.state.input}></textarea>
         </div>
-        <div id="preview-container">
+
+        <div id="preview-container" style={previewVis}>
           <div id="preview-header"> 
-            <p>Previewer</p> 
-            <i className="fa fa-arrows-alt hide-toggle" />
+            <p>Previewer {this.state.previewVis == "visible" ? "true" : "false"}</p> 
+            <i className={this.state.editorVis === "visible" ? "fa fa-arrows-alt hide-toggle" : "fa fa-compress hide-toggle"} style={previewVis} id="preview-toggle" onClick={this.handleClick}/>
           </div>
           <div id="preview-output">
             <div id="preview-output-margin">
-              <div dangerouslySetInnerHTML={this.markdown()}/> 
-
+              <div dangerouslySetInnerHTML={this.markdown()} /> 
             </div>
-            
           </div>
         </div>
       </div>
