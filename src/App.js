9449521fe8893e1dba596546bc424304f08bcdc5
tahmdev <qwertiimusic@gmail.com>
2022-03-08 17:49:14 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { marked } from "marked";
+import React from 'react';
+import Markdown from 'marked-react';
+import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Main />
     </div>
   );
+}
+
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      input: ""
+    }
+    this.markdown = this.markdown.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event){
+    this.setState({
+      
+      input: event.target.value
+    })
+  }
+  markdown(){
+    let rawMarkup = marked.parse(this.state.input, {breaks:true})
+    return {__html: rawMarkup};
+  }
+  render(){
+    return(
+      <div>
+        <div id="editor-container">
+          <div id="editor-header"> 
+            <p>Editor</p> 
+            <i className="fa fa-arrows-alt hide-toggle" />
+          </div>
+          <textarea id="editor-input" onChange={this.handleChange}></textarea>
+        </div>
+        <div id="preview-container">
+          <div id="preview-header"> 
+            <p>Previewer</p> 
+            <i className="fa fa-arrows-alt hide-toggle" />
+          </div>
+          <div id="preview-output">
+            <div id="preview-output-margin">
+              <div dangerouslySetInnerHTML={this.markdown()}/> 
+
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
